55a7e10b185b016726827b03d92181fb50c8c76c
Norgat <norg113@gmail.com>
2014-06-16 13:40:43 +0400

    Update project.clj for gpg singing.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/Norgat/quil-cljs-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :singing {:gpg-key "norg113@gmail.com"}
+  :deploy-repositories [["clojars" {:creds :gpg}]])
