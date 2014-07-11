e560907c3e064c9742fc5747a61a68f977b7974e
Norgat <norg113@gmail.com>
2014-07-11 12:21:11 +0400

    Update deps in generated project.clj.

--- src/leiningen/new/quil_cljs/project.clj
+++ src/leiningen/new/quil_cljs/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
   				      [org.clojars.norgat/quil "2.0.0-alpha"]
-  				      [org.clojure/clojurescript "0.0-2234"]]
+  				      [org.clojure/clojurescript "0.0-2268"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
   :hooks [leiningen.cljsbuild]
