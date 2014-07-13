f07f949b57f45cb2ccac36608ee740ec6ed132cf
Norgat <norg113@gmail.com>
2014-07-13 16:22:42 +0400

    Update deps and macros require approach.

--- src/leiningen/new/quil_cljs/core.cljs
+++ src/leiningen/new/quil_cljs/core.cljs
@@ -1,14 +1,12 @@
 (ns {{name}}.core
-	(:require [quil.core :as q])
-	(:use-macros [quil.core :only [defsketch]]))
+	(:require [quil.core :as q :include-macros true]))
 
 (defn draw []
 	(q/background 255)
 	(q/fill 0)
-
 	(q/ellipse 56 46 55 55))
 
-(defsketch {{name}}
+(q/defsketch {{name}}
 	:draw draw
 	:host "{{name}}"
 	:size [300 300])
--- src/leiningen/new/quil_cljs/project.clj
+++ src/leiningen/new/quil_cljs/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-  				      [org.clojars.norgat/quil "2.0.0-alpha"]
+  				      [quil "2.2.0-SNAPSHOT"]
   				      [org.clojure/clojurescript "0.0-2268"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
