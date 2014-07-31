bcb7d8c012667588f5a91e4c793490bc29146e06
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-07-31 23:27:40 +0100

    Replace tabs with spaces in cljs template.

--- cljs/src/leiningen/new/quil_cljs/core.cljs
+++ cljs/src/leiningen/new/quil_cljs/core.cljs
@@ -1,12 +1,12 @@
 (ns {{name}}.core
-	(:require [quil.core :as q :include-macros true]))
+  (:require [quil.core :as q :include-macros true]))
 
 (defn draw []
-	(q/background 255)
-	(q/fill 0)
-	(q/ellipse 56 46 55 55))
+  (q/background 255)
+  (q/fill 0)
+  (q/ellipse 56 46 55 55))
 
 (q/defsketch {{name}}
-	:draw draw
-	:host "{{name}}"
-	:size [300 300])
+  :draw draw
+  :host "{{name}}"
+  :size [300 300])
