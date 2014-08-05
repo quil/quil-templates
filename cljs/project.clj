1f4cf76e194238687a71e49046e0398469c54434
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-08-05 22:29:43 +0100

    Update templates to use quil 2.1.1

--- cljs/project.clj
+++ cljs/project.clj
@@ -1,8 +1,7 @@
-(defproject quil-cljs/lein-template "2.2.0.0"
+(defproject quil-cljs/lein-template "2.2.1.0"
   :description "Leiningen template for Quil ClojureScript project."
   :url "https://github.com/quil/quil-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :singing {:gpg-key "norg113@gmail.com"}
   :deploy-repositories [["clojars" {:creds :gpg}]])
