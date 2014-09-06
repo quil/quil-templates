5de9caaed216ed081a620108f96e5c25405a2bc6
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-09-06 16:04:51 +0100

    Add comments to templates.

---
+++ test_templates.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+PROJECT=testproject
+
+cd clj
+
+echo
+echo "Installing Clojure template."
+echo "----------------------------"
+lein install
+
+echo
+echo "Creating project using Clojure template."
+echo "----------------------------------------"
+lein new quil $PROJECT
+cd $PROJECT
+
+echo
+echo "Running Clojure project."
+echo "------------------------"
+lein run -m $PROJECT.core
+
+echo
+echo "Clojure project source."
+echo "-----------------------"
+less src/$PROJECT/core.clj
+
+cd ../../
+rm -rf clj/$PROJECT
+
+cd cljs
+
+echo
+echo "Installing ClojureScript template."
+echo "----------------------------------"
+lein install
+
+echo
+echo "Creating project using ClojureScript template."
+echo "----------------------------------------------"
+lein new quil-cljs $PROJECT
+cd $PROJECT
+
+echo
+echo "Building ClojureScript project."
+echo "-------------------------------"
+lein cljsbuild once
+google-chrome web/index.html
+
+echo
+echo "ClojureScript source."
+echo "---------------------"
+less src/$PROJECT/core.cljs
+
+cd ../../
+rm -rf cljs/$PROJECT
+
+
+echo
+echo "Done"
+echo "----"
+
+
