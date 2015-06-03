387ecb99230c6909ccaa9fd38b40b9e0064610b6
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-06-02 23:42:21 -0700

    Release 2.2.6

--- cljs/src/leiningen/new/quil_cljs/project.clj
+++ cljs/src/leiningen/new/quil_cljs/project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.5"]
-                 [org.clojure/clojurescript "0.0-2740"]]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [quil "2.2.6"]
+                 [org.clojure/clojurescript "0.0-3308"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.0.6"]]
   :hooks [leiningen.cljsbuild]
 
   :cljsbuild
