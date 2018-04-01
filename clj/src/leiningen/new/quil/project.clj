b7221bb5f29b1367f82d5936a104994de186859e
Nikita Beloglazov <nikelandjelo@gmail.com>
2018-04-01 00:48:28 -0700

    Release 2.7.1

--- clj/src/leiningen/new/quil/project.clj
+++ clj/src/leiningen/new/quil/project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.6.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [quil "2.7.1"]]
   :main {{name}}.core)
--- cljs/src/leiningen/new/quil_cljs/project.clj
+++ cljs/src/leiningen/new/quil_cljs/project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "2.6.0"]
-                 [org.clojure/clojurescript "1.9.946"]]
+                 [quil "2.7.1"]
+                 [org.clojure/clojurescript "1.10.238"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.14"]]
+            [lein-figwheel "0.5.15"]]
   :hooks [leiningen.cljsbuild]
 
   :cljsbuild
