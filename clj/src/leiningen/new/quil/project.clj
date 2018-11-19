c280f0bf23944b3045c99da53e87f8d1ac6536a8
Nikita Beloglazov <nikelandjelo@gmail.com>
2018-11-18 18:15:06 -0800

    Release 2.8.0

--- clj/src/leiningen/new/quil/project.clj
+++ clj/src/leiningen/new/quil/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "2.7.1"]])
+                 [quil "2.8.0"]])
--- cljs/src/leiningen/new/quil_cljs/project.clj
+++ cljs/src/leiningen/new/quil_cljs/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "2.7.1"]
-                 [org.clojure/clojurescript "1.10.238"]]
+                 [quil "2.8.0"]
+                 [org.clojure/clojurescript "1.10.439"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.15"]]
