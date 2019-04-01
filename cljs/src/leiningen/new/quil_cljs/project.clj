e8f29c2dfad06abb0b04f5229090e9ad04077972
Nikita Beloglazov <nikelandjelo@gmail.com>
2019-03-31 18:54:08 -0700

    Release 3.0.0.

--- cljs/src/leiningen/new/quil_cljs/project.clj
+++ cljs/src/leiningen/new/quil_cljs/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "3.0.0-SNAPSHOT"]
+                 [quil "3.0.0"]
                  [org.clojure/clojurescript "1.10.520"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
